eca4ebbb8eea4b7cb5cb3593b2ee56b4d4d14cd8
bargest <DIZz92@mail.ru>
2023-10-09 22:26:53 +0300

    Не тудой

--- src/item.py
+++ src/item.py
@@ -5,7 +5,6 @@ class Item:
     pay_rate = 1.0
     all = []
 
-
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -18,7 +17,6 @@ class Item:
         self.price = price
         self.quantity = quantity
 
-
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -27,7 +25,6 @@ class Item:
         """
         all_price = (self.quantity * self.price)
         return all_price
-
 
     def apply_discount(self) -> None:
         """
