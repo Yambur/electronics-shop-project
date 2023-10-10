5bded45faaeefad2bdcd7e36d9c4a683c774040b
bargest <DIZz92@mail.ru>
2023-10-10 23:02:08 +0300

    5 часов мучений закончены

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -31,3 +34,36 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path: str):
+        cls.all.clear()
+        items = []
+        with open(file_path, 'r', newline='') as file:
+            reader = csv.DictReader(file)
+            for i in reader:
+                name = i['name']
+                price = float(i['price']) if 'price' in i else 0.0
+                quantity = int(i['quantity']) if 'quantity' in i else 0
+                item = cls(name, price, quantity)
+                items.append(item)
+        cls.all.extend(items)
+        return items
+
+    @staticmethod
+    def string_to_number(string_num: str):
+        try:
+            if '.' in string_num:
+                return int(float(string_num))
+            else:
+                return int(string_num)
+        except ValueError:
+            return 0
