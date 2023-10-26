48bd8c9097c497629bfa3c31bec55ba0abb6455b
bargest <DIZz92@mail.ru>
2023-10-26 08:15:24 +0300

    add test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,5 @@
 from src.item import Item
+import pytest
 
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 
@@ -36,12 +37,31 @@ def test_string_to_number():
     assert Item.string_to_number("abc") == 0
     assert Item.string_to_number("") == 0
 
+
 def test_repr():
     item = Item('Смартфон', 10000, 20)
     test_repr = "Item('Смартфон', 10000, 20)"
     assert repr(item) == test_repr
 
+
 def test_str():
     item = Item('Холодильник', 10, 1)
     test_str = "Холодильник"
     assert str(item) == test_str
+
+
+@pytest.mark.parametrize("item1_quantity, item2_quantity, expected_result", [
+    (1, 2, 3),
+    (0, 5, 5),
+    (3, 3, 6),
+    (10, 20, 30)
+])
+def test_add_item(item1_quantity, item2_quantity, expected_result):
+    item1 = Item("item1", 10.0, item1_quantity)
+    item2 = Item("item2", 20.0, item2_quantity)
+    result = item1 + item2
+    assert result == expected_result
+
+    with pytest.raises(TypeError):
+        # Попробуйте сложить Item с чем-то другим
+        item1 + "some_string"
