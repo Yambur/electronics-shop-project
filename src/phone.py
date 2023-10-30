2271e27052724b1413c7e7252921171c4e05f1fa
bargest <DIZz92@mail.ru>
2023-10-30 06:34:36 +0300

    add property

--- src/phone.py
+++ src/phone.py
@@ -4,7 +4,17 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if value <= 0 or not isinstance(value, int):
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self._number_of_sim = value
 
     def __str__(self):
         return f'{self.name}'
